f374e52b4094e713ef3568280228ec7de2917fb9
Erick Masson <erickmasson00@gmail.com>
2026-07-29 20:31:23 -0300

    refactor(config): atualizar configuracao do redis template

--- src/main/java/com/payflow_engine/infra/config/RedisConfig.java
+++ src/main/java/com/payflow_engine/infra/config/RedisConfig.java
@@ -4,23 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    @SuppressWarnings("deprecation") // <--- Oculta o aviso amarelo na sua IDE
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
-        // As chaves serão Strings
         template.setKeySerializer(new StringRedisSerializer());
 
-        // Os valores serão convertidos para JSON usando o Jackson 2 (Padrão do Spring Boot 3)
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new GenericJacksonJsonRedisSerializer(objectMapper));
 
         return template;
     }
